241e1f07187969d9ea8b44fea434c266831084f9
Li Pan <l.pan.toronto@gmail.com>
2016-02-22 18:15:23 -0500

    Acceleration and deceleration working

--- Road/ViewController.m
+++ Road/ViewController.m
@@ -16,11 +16,6 @@
 @property (nonatomic, strong) UIWebView *bookContentView;
 @property (nonatomic, assign) NSUInteger spineIndex;
 
-@property (nonatomic, strong) NSTimer *timer;
-@property (nonatomic, strong) NSTimer *accelerationtimer;
-@property (nonatomic, strong) NSTimer *deccelerationtimer;
-
-
 @property (nonatomic, strong) CADisplayLink *displaylink;
 
 //Data
@@ -34,31 +29,41 @@
 
 //RunTime
 @property (nonatomic, assign) CFTimeInterval startTime;
+@property (nonatomic, assign) NSTimeInterval timeIntervalBetweenIndex;
 @property (nonatomic, assign) int wordIndex;
 
-//Speed
-@property (nonatomic, assign) float acceleration;
-@property (nonatomic, assign) float baseSpeed;
-@property (nonatomic, assign) float currentSpeed;
-@property (nonatomic, assign) float maxSpeed;
-
-@property (nonatomic, assign) NSTimeInterval timeIntervalBetweenIndex;
-
 @property (nonatomic, assign) BOOL accelerationBegan;
+@property (nonatomic, assign) BOOL breakingBegan;
+
+@property (nonatomic, strong) NSTimer *timer;
+@property (nonatomic, strong) NSTimer *accelerationtimer;
+@property (nonatomic, strong) NSTimer *deccelerationtimer;
+
+//Speeds
+@property (nonatomic, assign) float maxSpeed;
+@property (nonatomic, assign) float minSpeed;
+@property (nonatomic, assign) float acceleration;
+@property (nonatomic, assign) float deceleration;
+
 
 //Interaction
-@property (nonatomic, strong) UILongPressGestureRecognizer *gasPedal;
-
+@property (nonatomic, strong) UIView *breakPedal;
+@property (nonatomic, strong) UILabel *dot;
+@property (nonatomic, strong) UILongPressGestureRecognizer *breakPedalGesture;
+@property (nonatomic, assign) CGRect breakPedalFrame;
 
 @end
 
 @implementation ViewController
 
 -(void)viewDidAppear:(BOOL)animated {
-    //    self.displaylink = [CADisplayLink displayLinkWithTarget:self selector:@selector(update)];
-    //    [self.displaylink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+    self.displaylink = [CADisplayLink displayLinkWithTarget:self selector:@selector(updateFrame)];
+    [self.displaylink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
     
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:self.timeIntervalBetweenIndex target:self selector:@selector(update) userInfo:nil repeats:YES];
+    [self.timer invalidate];
+    self.timer = nil;
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:self.timeIntervalBetweenIndex target:self selector:@selector(update) userInfo:nil repeats:NO];
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
@@ -71,35 +76,42 @@
     [super viewDidLoad];
     [self loadBook];
     
-    
-    self.timeIntervalBetweenIndex = 0.5;
-    
-    self.acceleration = 0.0f;
-    self.baseSpeed = 0.5f;
-    self.maxSpeed = 0.3f;
+    self.minSpeed = 0.5;
+    self.maxSpeed = 0.1;
+    self.acceleration = 0.05;
+    self.deceleration = 0.005;
+    self.timeIntervalBetweenIndex = self.minSpeed;
     self.accelerationBegan = NO;
+    
     UIImage *paper = [UIImage imageNamed:@"ivoryPaper.png"];
+    UIImage *breakPedal = [UIImage imageNamed:@"finger_noise.png"];
+    
     self.view.layer.contents = (__bridge id)paper.CGImage;
     
-    //    UILabel *dot = [[UILabel alloc]initWithFrame:CGRectMake(CGRectGetMinX(self.view.frame), CGRectGetMaxY(self.view.frame)*(1-0.61803398875f), CGRectGetWidth(self.view.frame)*1.38196601125, 30.0f)];
     
-    UILabel *dot = [[UILabel alloc]initWithFrame:CGRectMake(CGRectGetWidth(self.view.frame)*0.68903398875f, CGRectGetMaxY(self.view.frame)*(1-0.57603398875f), 4.5f, 4.5f)];
-    dot.layer.cornerRadius = 3.0f;
-    dot.layer.borderWidth = 1.5f;
-    dot.clipsToBounds = YES;
-    dot.layer.borderColor = [UIColor colorWithRed:195.0f/255.0f green:25.0f/255.0f blue:25.0f/255.0f alpha:0.7f].CGColor;
+    self.dot = [[UILabel alloc]initWithFrame:CGRectMake(CGRectGetWidth(self.view.frame)*0.68903398875f, CGRectGetMaxY(self.view.frame)*(1-0.57603398875f), 4.5f, 4.5f)];
+    self.dot.layer.cornerRadius = 3.0f;
+    self.dot.layer.borderWidth = 1.5f;
+    self.dot.clipsToBounds = YES;
+    self.dot.layer.borderColor = [UIColor colorWithRed:195.0f/255.0f green:25.0f/255.0f blue:25.0f/255.0f alpha:0.7f].CGColor;
+    [self.view addSubview:self.dot];
     
-    [UIView animateWithDuration:self.timeIntervalBetweenIndex delay:0.0 options:UIViewKeyframeAnimationOptionRepeat animations:^{
-        dot.alpha = 0.0;
-    } completion:nil];
+    self.breakPedalGesture = [[UILongPressGestureRecognizer alloc]initWithTarget:self action:@selector(breaking)];
+    self.breakPedalFrame = CGRectMake(CGRectGetWidth(self.view.frame)*0.38196601125, CGRectGetMaxY(self.view.frame)/1.61803398875, 220.0f, 220.0f);
+    CGAffineTransform pedalPosition = CGAffineTransformIdentity;
+    pedalPosition = CGAffineTransformScale(pedalPosition, 0.50f, 0.50f);
+    pedalPosition = CGAffineTransformRotate(pedalPosition, M_PI/180.0 * -37);
+    pedalPosition = CGAffineTransformTranslate(pedalPosition, 50.0f, 0.0f);
+    self.breakPedal = [[UIView alloc]initWithFrame:self.breakPedalFrame];
+    self.breakPedal.layer.contents = (__bridge id)breakPedal.CGImage;
+    self.breakPedal.layer.opacity = 0.2f;
+    self.breakPedal.layer.affineTransform = pedalPosition;
+    self.breakPedal.layer.shadowColor = [UIColor blackColor].CGColor;
     
-    [self.view addSubview:dot];
+    [self.breakPedal addGestureRecognizer:self.breakPedalGesture];
     
+    [self.view addSubview:self.breakPedal];
     
-    
-    
-    
-    //    self.view.backgroundColor = [UIColor colorWithRed:208.0f/255.0f green:178.0f/255.0f blue:118.0f/255.0f alpha:1.0f];
 }
 
 - (void)loadBook {
@@ -241,58 +253,78 @@
 }
 
 - (void)update {
-    
-    //    self.wordIndex = ((self.displaylink.timestamp - self.startTime)/(self.baseSpeed + self.acceleration));
-    //
-    //    if (self.acceleration >= self.baseSpeed - 0.01) {
-    //        self.acceleration = self.baseSpeed;
-    //    } else if (self.currentSpeed >= self.baseSpeed) {
-    //        self.acceleration = 0;
-    //    }
-    //
-    //    if (self.accelerationBegan) {
-    //        self.acceleration -= 0.001;
-    //    } else if (!self.accelerationBegan) {
-    //
-    //        self.acceleration += 0.005;
-    //    }
-    
-    
-    self.wordIndex ++;
+    if (self.timeIntervalBetweenIndex < 1) {
+        self.wordIndex ++;
+    } else if (self.timeIntervalBetweenIndex >= 1) {
+        self.wordIndex --;
+    }
     self.focusText.text = [self.wordsArray objectAtIndex:self.wordIndex];
     if (self.wordIndex == self.wordsArray.count) {
         self.displaylink.paused = YES;
     }
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:self.timeIntervalBetweenIndex target:self selector:@selector(update) userInfo:nil repeats:NO];
+    //    NSLog(@"%d, %lu, %0.2f", self.wordIndex, (unsigned long)self.wordsArray.count, self.timeIntervalBetweenIndex);
     
-    self.currentSpeed = self.baseSpeed + self.acceleration;
-    
-//    NSLog(@"%d, %0.2f, %0.3f", self.wordIndex, self.acceleration, self.currentSpeed);
-    //    NSLog(@"%d", self.wordsArray.count);
+    self.dot.alpha = 0.8;
+    [UIView animateWithDuration:self.timeIntervalBetweenIndex delay:0.0 options:UIViewKeyframeAnimationOptionRepeat animations:^{
+        self.dot.alpha = 0.0;
+    } completion:nil];
 }
 
 - (void)modifyTimeInterval: (float)time {
-
-    self.timeIntervalBetweenIndex += time;
+    if (!self.breakingBegan) {
+        self.timeIntervalBetweenIndex += time;
+        self.timeIntervalBetweenIndex = MAX(self.timeIntervalBetweenIndex, self.maxSpeed);
+        self.timeIntervalBetweenIndex = MIN(self.timeIntervalBetweenIndex, self.minSpeed);
+    } else if (self.breakingBegan) {
+        self.timeIntervalBetweenIndex += time;
+        self.timeIntervalBetweenIndex = MAX(self.timeIntervalBetweenIndex, self.maxSpeed);
+        self.timeIntervalBetweenIndex = MIN(self.timeIntervalBetweenIndex, 1.10f);
+    }
+        NSLog(@"%f %d", self.timeIntervalBetweenIndex, self.wordIndex);
     
-    self.timeIntervalBetweenIndex = MAX(self.timeIntervalBetweenIndex, 0.05f);
-    self.timeIntervalBetweenIndex = MIN(self.timeIntervalBetweenIndex, 0.49f);
-    NSLog(@"%f", self.timeIntervalBetweenIndex);
-    [self.timer invalidate];
-    self.timer = nil;
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:self.timeIntervalBetweenIndex target:self selector:@selector(update) userInfo:nil repeats:YES];
+//    if (self.timeIntervalBetweenIndex >= 0.50f) {
+//        [self.deccelerationtimer invalidate];
+//        self.deccelerationtimer = nil;
+//    }
 }
 
--(void)modifySpeed {
-    if (self.accelerationBegan) {
-        [self modifyTimeInterval:-0.1];
-    } else if (!self.accelerationBegan) {
-            [self modifyTimeInterval:+0.01];
+- (void)updateFrame {
+    if (self.breakPedalGesture.state == UIControlEventTouchDown) {
+        [UIView animateWithDuration:4.0f animations:^{
+            self.breakPedal.alpha = 0.50f;
+            self.breakPedal.layer.shadowOpacity = 2.60f;
+            self.breakPedal.layer.shadowOffset = CGSizeMake(2.0f, 5.0f);
+            self.breakPedal.layer.shadowRadius = 30.0f;
+        }];
+        
+    } else if (!self.breakPedalGesture.state == UIControlEventTouchDown) {
+        self.breakPedal.alpha = MAX(self.breakPedal.alpha, 0.2);
+        self.breakPedal.alpha -= 0.01f;
+        self.breakPedal.layer.shadowOpacity -= 0.1f;
+        self.breakPedal.layer.shadowOpacity = MIN(self.breakPedal.layer.shadowOpacity, 0.0);
+        self.breakPedal.layer.shadowRadius -= 0.5f;
+        self.breakPedal.layer.shadowRadius = MIN(self.breakPedal.layer.shadowRadius, 0.0);
     }
+}
+
+- (void)modifySpeed {
+    if (self.accelerationBegan) {
+        [self modifyTimeInterval:-self.acceleration];
+    } else if (!self.accelerationBegan) {
+        [self modifyTimeInterval:+self.deceleration];
+    }
+}
+
+- (void)startBreaking {
+    [self modifyTimeInterval:+0.020];
+    NSLog(@"%f", self.timeIntervalBetweenIndex);
+    
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     self.accelerationBegan = YES;
-    self.accelerationtimer = [NSTimer scheduledTimerWithTimeInterval: 0.5 target:self selector:@selector(modifySpeed) userInfo:nil repeats:YES];
+    self.accelerationtimer = [NSTimer scheduledTimerWithTimeInterval: 0.10f target:self selector:@selector(modifySpeed) userInfo:nil repeats:YES];
     NSLog(@"Began %d", self.accelerationBegan);
 }
 
@@ -300,9 +332,27 @@
     self.accelerationBegan = NO;
     [self.accelerationtimer invalidate];
     self.accelerationtimer = nil;
-    self.accelerationtimer = [NSTimer scheduledTimerWithTimeInterval: 0.1 target:self selector:@selector(modifySpeed) userInfo:nil repeats:YES];
-    
+    self.deccelerationtimer = [NSTimer scheduledTimerWithTimeInterval: 0.10f target:self selector:@selector(modifySpeed) userInfo:nil repeats:YES];
     NSLog(@"Ended %d", self.accelerationBegan);
+}
+
+-(void)breaking {
+    if (self.breakPedalGesture.state == UIControlEventTouchDown) {
+        NSLog(@"break");
+        self.accelerationBegan = NO;
+        [self.accelerationtimer invalidate];
+        self.accelerationtimer = nil;
+        self.breakingBegan = YES;
+        self.deccelerationtimer = [NSTimer scheduledTimerWithTimeInterval: 0.10f target:self selector:@selector(startBreaking) userInfo:nil repeats:YES];
+        NSLog(@"%f", self.timeIntervalBetweenIndex);
+        
+    } if (self.breakPedalGesture.state == UIGestureRecognizerStateEnded) {
+        NSLog(@"break ended");
+        self.breakingBegan = NO;
+        self.deccelerationtimer = [NSTimer scheduledTimerWithTimeInterval: 0.10f target:self selector:@selector(modifySpeed) userInfo:nil repeats:YES];
+    }
+    
+    
 }
 
 @end
