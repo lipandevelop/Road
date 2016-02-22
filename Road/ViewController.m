c0ff3167829eaec563915ef975f0d3fab9eadeb1
Li Pan <l.pan.toronto@gmail.com>
2016-02-22 12:40:13 -0500

    Acceleration Working

--- Road/ViewController.m
+++ Road/ViewController.m
@@ -16,6 +16,11 @@
 @property (nonatomic, strong) UIWebView *bookContentView;
 @property (nonatomic, assign) NSUInteger spineIndex;
 
+@property (nonatomic, strong) NSTimer *timer;
+@property (nonatomic, strong) NSTimer *accelerationtimer;
+@property (nonatomic, strong) NSTimer *deccelerationtimer;
+
+
 @property (nonatomic, strong) CADisplayLink *displaylink;
 
 //Data
@@ -37,6 +42,8 @@
 @property (nonatomic, assign) float currentSpeed;
 @property (nonatomic, assign) float maxSpeed;
 
+@property (nonatomic, assign) NSTimeInterval timeIntervalBetweenIndex;
+
 @property (nonatomic, assign) BOOL accelerationBegan;
 
 //Interaction
@@ -48,13 +55,24 @@
 @implementation ViewController
 
 -(void)viewDidAppear:(BOOL)animated {
-    self.displaylink = [CADisplayLink displayLinkWithTarget:self selector:@selector(update)];
-    [self.displaylink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+    //    self.displaylink = [CADisplayLink displayLinkWithTarget:self selector:@selector(update)];
+    //    [self.displaylink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:self.timeIntervalBetweenIndex target:self selector:@selector(update) userInfo:nil repeats:YES];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [self.timer invalidate];
+    self.timer = nil;
+    
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self loadBook];
+    
+    
+    self.timeIntervalBetweenIndex = 0.5;
     
     self.acceleration = 0.0f;
     self.baseSpeed = 0.5f;
@@ -62,9 +80,26 @@
     self.accelerationBegan = NO;
     UIImage *paper = [UIImage imageNamed:@"ivoryPaper.png"];
     self.view.layer.contents = (__bridge id)paper.CGImage;
-
     
-//    self.view.backgroundColor = [UIColor colorWithRed:208.0f/255.0f green:178.0f/255.0f blue:118.0f/255.0f alpha:1.0f];
+    //    UILabel *dot = [[UILabel alloc]initWithFrame:CGRectMake(CGRectGetMinX(self.view.frame), CGRectGetMaxY(self.view.frame)*(1-0.61803398875f), CGRectGetWidth(self.view.frame)*1.38196601125, 30.0f)];
+    
+    UILabel *dot = [[UILabel alloc]initWithFrame:CGRectMake(CGRectGetWidth(self.view.frame)*0.68903398875f, CGRectGetMaxY(self.view.frame)*(1-0.57603398875f), 4.5f, 4.5f)];
+    dot.layer.cornerRadius = 3.0f;
+    dot.layer.borderWidth = 1.5f;
+    dot.clipsToBounds = YES;
+    dot.layer.borderColor = [UIColor colorWithRed:195.0f/255.0f green:25.0f/255.0f blue:25.0f/255.0f alpha:0.7f].CGColor;
+    
+    [UIView animateWithDuration:self.timeIntervalBetweenIndex delay:0.0 options:UIViewKeyframeAnimationOptionRepeat animations:^{
+        dot.alpha = 0.0;
+    } completion:nil];
+    
+    [self.view addSubview:dot];
+    
+    
+    
+    
+    
+    //    self.view.backgroundColor = [UIColor colorWithRed:208.0f/255.0f green:178.0f/255.0f blue:118.0f/255.0f alpha:1.0f];
 }
 
 - (void)loadBook {
@@ -160,10 +195,10 @@
     
     
     //Label
-    self.focusText = [[UILabel alloc]initWithFrame:CGRectMake(CGRectGetMinX(self.view.frame), CGRectGetMaxY(self.view.frame)*(1-0.61803398875f), CGRectGetWidth(self.view.frame), 30.0f)];
-//    self.focusText.layer.shadowOffset = CGSizeMake(0, 1);
-//    self.focusText.layer.shadowRadius = 2.5f;
-//    self.focusText.layer.shadowOpacity = 0.3;
+    self.focusText = [[UILabel alloc]initWithFrame:CGRectMake(CGRectGetWidth(self.view.frame)*0.38196601125, CGRectGetMaxY(self.view.frame)*(1-0.61803398875f), 200, 30.0f)];
+    //    self.focusText.layer.shadowOffset = CGSizeMake(0, 1);
+    //    self.focusText.layer.shadowRadius = 2.5f;
+    //    self.focusText.layer.shadowOpacity = 0.3;
     self.focusText.numberOfLines = 0;
     self.focusText.text = self.bookTextString;
     self.focusText.font = [UIFont fontWithName:(@"AmericanTypewriter") size:16];
@@ -206,19 +241,24 @@
 }
 
 - (void)update {
-    if (self.acceleration >= self.baseSpeed - 0.01) {
-        self.acceleration = self.baseSpeed;
-    } else if (self.currentSpeed >= self.baseSpeed) {
-        self.acceleration = 0;
-    }
     
-    if (self.accelerationBegan) {
-        self.acceleration -= 0.001;
-    } else if (!self.accelerationBegan) {
-        self.acceleration += 0.005;
-    }
+    //    self.wordIndex = ((self.displaylink.timestamp - self.startTime)/(self.baseSpeed + self.acceleration));
+    //
+    //    if (self.acceleration >= self.baseSpeed - 0.01) {
+    //        self.acceleration = self.baseSpeed;
+    //    } else if (self.currentSpeed >= self.baseSpeed) {
+    //        self.acceleration = 0;
+    //    }
+    //
+    //    if (self.accelerationBegan) {
+    //        self.acceleration -= 0.001;
+    //    } else if (!self.accelerationBegan) {
+    //
+    //        self.acceleration += 0.005;
+    //    }
     
-    self.wordIndex = ((self.displaylink.timestamp - self.startTime)/(self.baseSpeed + self.acceleration));
+    
+    self.wordIndex ++;
     self.focusText.text = [self.wordsArray objectAtIndex:self.wordIndex];
     if (self.wordIndex == self.wordsArray.count) {
         self.displaylink.paused = YES;
@@ -226,19 +266,42 @@
     
     self.currentSpeed = self.baseSpeed + self.acceleration;
     
-    NSLog(@"%d, %0.2f, %0.3f", self.wordIndex, self.acceleration, self.currentSpeed);
+//    NSLog(@"%d, %0.2f, %0.3f", self.wordIndex, self.acceleration, self.currentSpeed);
+    //    NSLog(@"%d", self.wordsArray.count);
 }
 
-- (void)gas {
+- (void)modifyTimeInterval: (float)time {
+
+    self.timeIntervalBetweenIndex += time;
+    
+    self.timeIntervalBetweenIndex = MAX(self.timeIntervalBetweenIndex, 0.05f);
+    self.timeIntervalBetweenIndex = MIN(self.timeIntervalBetweenIndex, 0.49f);
+    NSLog(@"%f", self.timeIntervalBetweenIndex);
+    [self.timer invalidate];
+    self.timer = nil;
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:self.timeIntervalBetweenIndex target:self selector:@selector(update) userInfo:nil repeats:YES];
+}
+
+-(void)modifySpeed {
+    if (self.accelerationBegan) {
+        [self modifyTimeInterval:-0.1];
+    } else if (!self.accelerationBegan) {
+            [self modifyTimeInterval:+0.01];
+    }
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     self.accelerationBegan = YES;
+    self.accelerationtimer = [NSTimer scheduledTimerWithTimeInterval: 0.5 target:self selector:@selector(modifySpeed) userInfo:nil repeats:YES];
     NSLog(@"Began %d", self.accelerationBegan);
 }
 
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     self.accelerationBegan = NO;
+    [self.accelerationtimer invalidate];
+    self.accelerationtimer = nil;
+    self.accelerationtimer = [NSTimer scheduledTimerWithTimeInterval: 0.1 target:self selector:@selector(modifySpeed) userInfo:nil repeats:YES];
+    
     NSLog(@"Ended %d", self.accelerationBegan);
 }
 
